eef06c7a5a6c728a63bbda20a1bf5d760268afdc
EgyFalseX <mohamed.aly.omer@gmail.com>
2019-10-04 09:29:45 +0200

    Add project files.

---
+++ SqlBackup/BackupSrv.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SqlBackup
+{
+    public partial class BackupSrv : ServiceBase
+    {
+        Timer tmr;
+        DateTime StartTime;
+        //DateTime ExecuteEveryTime;
+        public BackupSrv()
+        {
+            InitializeComponent();
+        }
+        
+        protected override void OnStart(string[] args)
+        {
+            tmr = new Timer(60 * 1000);
+            tmr.Elapsed += Tmr_Elapsed;
+            StartTime = DateTime.Now.Date + Properties.Settings.Default.StartDateTime;
+            tmr.Start();
+        }
+
+        protected override void OnStop()
+        {
+            tmr.Stop();
+        }
+
+        private void Tmr_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SqlBackup/DBBackup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseBackup
+{
+    public class DBBackup
+    {
+        public DBBackup(string connectionString, string backupPath)
+        {
+            ConnectionString = connectionString;
+            BackupPath = backupPath;
+        }
+        public string ConnectionString { get; set; }
+        public string BackupPath { get; set; }
+
+        public async Task<bool> CreateBackupAsync()
+        {
+            return await Task.Run(() => 
+            {
+                SqlConnectionStringBuilder sqlConStrBuilder = new SqlConnectionStringBuilder(ConnectionString);
+                string backupFileName = string.Format("{0}{1}-{2}.bak", BackupPath, sqlConStrBuilder.InitialCatalog, DateTime.Now.ToString("yyyyMMddmmss"));
+                try
+                {
+                    using (var connection = new SqlConnection(sqlConStrBuilder.ConnectionString))
+                    {
+                        var query = string.Format("BACKUP DATABASE {0} TO DISK='{1}' WITH COMPRESSION",
+                            sqlConStrBuilder.InitialCatalog, backupFileName);
+
+                        using (var command = new SqlCommand(query, connection))
+                        {
+                            connection.Open();
+                            command.ExecuteNonQuery();
+                            return true;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+                return false;
+            });
+        }
+    }
+}
